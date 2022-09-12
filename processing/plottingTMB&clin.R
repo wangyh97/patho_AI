b82d9c24122aa97d3d2874a2775a4e34f024291d
wamgyh <medphdwyh@outlook.com>
2022-09-12 23:40:52 +0800

    plot

--- processing/plottingTMB&clin.R
+++ processing/plottingTMB&clin.R
@@ -1,12 +1,14 @@
-
+install.packages('magrittr')
+install.packages('tidyverse')
+library(magrittr)
+library(tidyverse)
+dev.off()
 clin_TMB <- read.csv('~/uro_biomarker/patho_AI/config/merged_clinical_data_with_TMB.csv')
-sorted <- subset(clin_TMB,clin_TMB$treatment_type=='Pharmaceutical Therapy, NOS' & clin_TMB$days_to_death!="'--") 
-
-ID <- sorted$case_submitter_id
-day_to_death <- sorted$days_to_death
-TMB <- sorted$TMB
+sorted <- subset(clin_TMB,clin_TMB$treatment_type=='Pharmaceutical Therapy, NOS' & clin_TMB$days_to_death!="'--")
+final <- select(sorted,ID,TMB,days_to_death)
+write.table(final,file = './config/TMB&D2D.txt',sep = '')
 
 
-plot(TMB,day_to_death,type='p',xlab = 'TMB',ylab = 'day_to_death')
-png(file = 'plot.png')
+
+
 dev.off()
