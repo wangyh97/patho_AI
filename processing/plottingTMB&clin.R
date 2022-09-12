7fa8781742b2dbe623288c39ba362cd598a55df5
wamgyh <medphdwyh@outlook.com>
2022-09-12 23:45:34 +0800

    plot

--- processing/plottingTMB&clin.R
+++ processing/plottingTMB&clin.R
@@ -6,7 +6,7 @@ dev.off()
 clin_TMB <- read.csv('~/uro_biomarker/patho_AI/config/merged_clinical_data_with_TMB.csv')
 sorted <- subset(clin_TMB,clin_TMB$treatment_type=='Pharmaceutical Therapy, NOS' & clin_TMB$days_to_death!="'--")
 final <- select(sorted,ID,TMB,days_to_death)
-write.table(final,file = './config/TMB&D2D.txt',sep = '')
+write.table(final,file = './config/TMB&D2D.txt',sep = ' ',quote = F)
 
 
 
