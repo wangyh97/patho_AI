ff94973f03dd07ce15c41c58e7927ca6b71057b3
wangyh97 <medphdwyh@outlook.com>
2023-03-13 09:09:05 +0800

    recommit dataseg,without changes

--- processing/CN_batch.sh
+++
@@ -1,3 +0,0 @@
-for i in {1..4};do
-    nohup python ColorNormalization.py -n 97  -i ${i}  >& ${i}_CN.out & 
-done
--- processing/batch.sh
+++
@@ -1,3 +0,0 @@
-for i in {1..10};do
-    nohup python extract_patches.py -n 39  -i ${i}  >& ${i}_CN.out & 
-done
